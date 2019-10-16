fd91e50c36dd139170042f4957aa542740a6b287
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2019-10-16 14:26:39 +0200

    Primer commit

---
+++ src/primergit/PrimerGit.java
@@ -0,0 +1,12 @@
+
+package primergit;
+//@author irodriguezsteuerberg
+ 
+public class PrimerGit {
+
+
+    public static void main(String[] args) {
+System.out.println("Hola mundo! Primer git");  
+    }
+    
+}
